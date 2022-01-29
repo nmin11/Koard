61a10f3377bf512df3a0e83979ab27114739c082
nmin11 <nmin1124@gmail.com>
2022-01-30 00:03:41 +0900

    Edit : separate own board list page from index.html

--- src/main/kotlin/practice/kotlin/koard/controller/BoardController.kt
+++ src/main/kotlin/practice/kotlin/koard/controller/BoardController.kt
@@ -5,18 +5,19 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import practice.kotlin.koard.entity.Board
 import practice.kotlin.koard.service.BoardService
 
 @Controller
-class HomeController(private val boardService: BoardService) {
+class BoardController(private val boardService: BoardService) {
 
-    @GetMapping("/")
-    fun index(model: Model): String {
-        val pageRequest: PageRequest = PageRequest.of(1, 5)
+    @GetMapping("/board/{page}")
+    fun index(@PathVariable("page")page: Int, model: Model): String {
+        val pageRequest: PageRequest = PageRequest.of(page, 5)
         val boards: Page<Board> = boardService.findAll(pageRequest)
         model.addAttribute("boards", boards)
-        return "index"
+        return "board/list"
     }
 
 }
